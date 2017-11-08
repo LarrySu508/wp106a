857f85ad424f72abca3a3d69d3c4dc91cfadb157
LarrySu508 <jack86112407@gmail.com>
2017-11-08 15:29:24 +0800

    adder new

--- example/06-editor/a/editor.js
+++ example/06-editor/a/editor.js
@@ -37,18 +37,8 @@ const template = [
           var text = document.getElementById('text')
           fs.writeFile(fileName, text.value)
         }
-      },
-      {label: 'Exit',role:'close'},
-      { type: 'separator' },
-      {
-        label: 'Open New File'
-        
-
-      },
-      {
-        label: 'Save As...'
-
-      }
+      }, 
+      { label: 'Exit', role: 'close' }
     ]
   },
   {
